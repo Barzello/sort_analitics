4ba6710e9f9f83f02a367f0ff567ace25adc5bdc
Barzello <reuzaki47@gmail.com>
2021-04-22 10:25:46 +0300

    optimazed work(without globals delta)
    Signed-off-by: Barzello <reuzaki47@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,21 @@
 #include <ctime>
 #include <io.h>
 #include "sysinfoapi.h"
+/* User libraries ------------------------------------------------------------*/
 #include "sort_list.hpp"
-using namespace std;
 /* Private defines -----------------------------------------------------------*/
 #define MAX_OUTPUT_DATA                         (1000)
 #define MAX_GENERATED_NUM_OF_VALUES             (20000)
 /* Private variables ---------------------------------------------------------*/
+using namespace std;
+
 int made[MAX_GENERATED_NUM_OF_VALUES]; //storage to work with random values
 int arr[MAX_GENERATED_NUM_OF_VALUES]; //storage for start random values
-
-DWORD bubblesort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD combsort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD insertionsort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD treesort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD gnomesort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD shakersort_delta[MAX_OUTPUT_DATA]={0,};
-DWORD selectionsort_delta[MAX_OUTPUT_DATA]={0,};
 /* Private function prototypes -----------------------------------------------*/
 
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-
 int randint() //function to generate random values
 {
     return rand() * RAND_MAX + rand();
@@ -71,8 +64,9 @@ void writeFile(DWORD *delta, string section) //function used to create txt file
     fsetpos(stdout, &pos);
 }
 
-void start_sort_experiment(DWORD *input_delta, void (*fun_ptr)(int* l, int* r), string filename)
+void start_sort_experiment(void (*fun_ptr)(int* l, int* r), string filename)
 {
+    DWORD input_delta[MAX_OUTPUT_DATA]={0,};
 	for(uint32_t i=0;i<MAX_OUTPUT_DATA;i++)
 	{
         if((i*100)>MAX_GENERATED_NUM_OF_VALUES) break;
@@ -89,12 +83,12 @@ void start_sort_experiment(DWORD *input_delta, void (*fun_ptr)(int* l, int* r), 
 int main(void)
 {
     random_generator(MAX_GENERATED_NUM_OF_VALUES);
-    start_sort_experiment(bubblesort_delta,bubblesort,"bubblesort");
-    start_sort_experiment(combsort_delta,combsort,"combsort");
-    start_sort_experiment(insertionsort_delta,insertionsort,"insertionsort");
-    start_sort_experiment(treesort_delta,treesort,"treesort");
-    start_sort_experiment(gnomesort_delta,gnomesort,"gnomesort");
-    start_sort_experiment(shakersort_delta,shakersort,"shakersort");
-    start_sort_experiment(selectionsort_delta,selectionsort,"selectionsort");
+    start_sort_experiment(bubblesort,"bubblesort");
+    //start_sort_experiment(combsort_delta,combsort,"combsort");
+    // start_sort_experiment(insertionsort_delta,insertionsort,"insertionsort");
+    // start_sort_experiment(treesort_delta,treesort,"treesort");
+    // start_sort_experiment(gnomesort_delta,gnomesort,"gnomesort");
+    // start_sort_experiment(shakersort_delta,shakersort,"shakersort");
+    // start_sort_experiment(selectionsort_delta,selectionsort,"selectionsort");
     return 0;
 }
